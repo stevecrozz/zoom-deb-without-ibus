a4aeb28f4369872db291ed3fae5a3c5c45f31918
Stephen Crosby <stevecrozz@gmail.com>
2025-03-25 15:45:35 -0700

    Always get latest zoom

--- repack_zoom_amd64_deb.sh
+++ repack_zoom_amd64_deb.sh
@@ -18,9 +18,9 @@ sed -i 's/ibus,\? \?//' control
 echo "Creating new control file without ibus dependency"
 tar --owner=0:0 --group=0:0 -c -a -f control.tar.xz ./control ./postinst ./postrm ./md5sums
 
-echo "Cleaning up temporary files"
-rm control postinst postrm md5sums
-rm zoom_amd64.deb
-
 echo "Repacking zoom_amd64.deb"
-ar r zoom_amd64.deb debian-binary control.tar.xz data.tar.xz 2>&1 > /dev/null
+ar r zoom_amd64_repacked.deb debian-binary control.tar.xz data.tar.xz 2>&1 > /dev/null
+
+echo "Cleaning up temporary files"
+rm -f control postinst postrm md5sums _gpgbuilder control.tar.xz data.tar.xz debian-binary zoom_amd64.deb
+
