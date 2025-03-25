94e022c0755209b7c047c1bac1477b9f7d50e0dd
Stephen Crosby <stevecrozz@gmail.com>
2025-03-25 15:08:24 -0700

    Commit an initial script for zoom 6.4.1.587

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # zoom-deb-without-ibus
-Downloads and repacks zoom_amd64.deb without ibus dependency
+
+Downloads and repacks zoom_amd64.deb without ibus dependency. Installing ibus
+gives me puzzing keyboard errors and upgrading zoom requires me to do this
+dance every time I upgrade, so I have scripted my solution.
---
+++ repack_zoom_amd64_deb.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+set -e
+
+echo "Downloading zoom_amd64.deb"
+curl --output zoom_amd64.deb --location --silent "https://zoom.us/client/6.4.1.587/zoom_amd64.deb"
+
+echo "Unpacking zoom_amd64.deb"
+ar x zoom_amd64.deb
+
+echo "Unpacking control file"
+tar xf control.tar.xz
+rm control.tar.xz
+
+echo "Stripping ibus dependenecy from control file"
+sed -i 's/ibus,\? \?//' control
+
+echo "Creating new control file without ibus dependency"
+tar --owner=0:0 --group=0:0 -c -a -f control.tar.xz ./control ./postinst ./postrm ./md5sums
+
+echo "Cleaning up temporary files"
+rm control postinst postrm md5sums
+rm zoom_amd64.deb
+
+echo "Repacking zoom_amd64.deb"
+ar r zoom_amd64.deb debian-binary control.tar.xz data.tar.xz 2>&1 > /dev/null
